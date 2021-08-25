2f25b7d6737f94fa46b56ca3e6ced4cd2eb509f9
Sarek Høverstad Skotåm <sarekhs@uio.no>
2021-08-25 21:32:15 +0200

    Swapped for loops with while loops. 1 for loop left(HashSet iterator)

--- src/solver.rs
+++ src/solver.rs
@@ -4,52 +4,80 @@ use crate::types::*;
 
 fn consistent(clauses: &Clauses) -> bool {
     let mut seen = HashSet::new();
-    for clause in clauses {
-        for literal in clause {
+    let mut i = 0;
+    let clauses_len = clauses.len();
+    while i < clauses_len {
+        let mut j = 0;
+        let clause = &clauses[i];
+        let clause_len = clause.len();
+        while j < clause_len {
+            let literal = &clause[j];
             if seen.contains(&-*literal) {
                 return false;
             }
             seen.insert(literal);
+            j += 1;
         }
+        i += 1;
     }
     return true;
 }
 
 fn contains_empty(clauses: &Clauses) -> bool {
-    for clause in clauses {
+    let mut i = 0;
+    let clauses_len = clauses.len();
+    while i < clauses_len {
+        let clause = &clauses[i];
         if clause.len() == 0 {
             return true;
         }
+        i += 1;
     }
     return false;
 }
 
 fn unit_propagate(clauses: &Clauses, literal: Literal) -> Clauses {
     let mut out_clauses = vec![];
-    for clause in clauses {
+    let mut i = 0;
+    let clauses_len = clauses.len();
+    while i < clauses_len {
+        let clause = &clauses[i];
         if !clause.contains(&literal) {
             let mut out_clause = vec![];
-            for lit in clause {
+            let mut j = 0;
+            let clause_len = clause.len();
+            while j < clause_len {
+                let lit = &clause[j];
                 if lit != &-literal {
                     out_clause.push(*lit);
                 }
+                j += 1;
             }
             out_clauses.push(out_clause);
         }
+        i += 1;
     }
     return out_clauses;
 }
 
 fn pure_literal_assign(clauses: &Clauses, literal: Literal) -> Clauses {
     let mut out_clauses = vec![];
-    for clause in clauses {
+    let mut i = 0;
+    let clauses_len = clauses.len();
+    while i < clauses_len {
+        let clause = &clauses[i];
         if !clause.contains(&literal) {
             let mut out_clause = vec![];
-            for lit in clause {
+            let mut j = 0;
+            let clause_len = clause.len();
+            while j < clause_len {
+                let lit = &clause[j];
                 out_clause.push(*lit);
+                j += 1;
             }
             out_clauses.push(out_clause);
         }
+        i += 1;
     }
     return out_clauses;
 }
@@ -59,6 +87,7 @@ fn choose_literal(_clauses: &Clauses, clause_counter: i32) -> Literal {
 }
 
 pub fn dpll(clauses: &mut Clauses, clause_counter: i32) -> bool {
+    let mut i;
     if contains_empty(clauses) {
         return false;
     }
@@ -68,28 +97,43 @@ pub fn dpll(clauses: &mut Clauses, clause_counter: i32) -> bool {
     let mut stabilized = false;
     while !stabilized {
         stabilized = true;
-        for i in 0..clauses.len() {
+        i = 0;
+        let clauses_len = clauses.len();
+        while i < clauses_len {
             if clauses[i].len() == 1 {
                 *clauses = unit_propagate(&clauses, clauses[i][0]);
                 stabilized = false;
                 break;
             }
+            i += 1;
         }
     }
     let mut pures = vec![];
     let mut seen = HashSet::new();
-    for i in 0..clauses.len() {
-        for &literal in &clauses[i] {
-            seen.insert(literal);
+    i = 0;
+    let clauses_len = clauses.len();
+    while i < clauses_len {
+        let mut j = 0;
+        let clause = &clauses[i];
+        let clause_len = clause.len();
+        while j < clause_len {
+            let literal = &clause[j];
+            seen.insert(*literal);
+            j += 1;
         }
+        i += 1;
     }
     for literal in &seen {
         if !seen.contains(&-*literal) {
             pures.push(*literal);
         }
     }
-    for literal in pures {
+    i = 0;
+    let pures_len = pures.len();
+    while i < pures_len {
+        let literal = pures[i];
         *clauses = pure_literal_assign(&clauses, literal);
+        i += 1;
     }
     let literal = choose_literal(&clauses, clause_counter);
     let new_counter = clause_counter + 1;
