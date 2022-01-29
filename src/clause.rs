c5a30b3429fdb943b70bb0c3f84cd3ab40708ae2
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2022-01-29 16:53:52 +0100

    Made check_if_unit pass

--- src/clause.rs
+++ src/clause.rs
@@ -27,7 +27,7 @@ pub fn sat_clause_inner(a: Seq<AssignedState>, c: Clause) -> bool {
                 /*
             (!(a[@(@c)[i].idx] === AssignedState::Unset) &&
                 (a[@(@c)[i].idx] === bool_to_assignedstate((@c)[i].polarity)))
-                */
+            */
             match a[@(@c)[i].idx] {
                 AssignedState::Positive => (@c)[i].polarity,
                 AssignedState::Negative => !(@c)[i].polarity,
@@ -120,54 +120,55 @@ impl Clause {
 
     #[predicate]
     pub fn invariant(self, n: Int) -> bool {
+        // Should remove the possibility of empty clauses
         pearlite! { self.vars_in_range(n) && self.no_duplicate_indexes() }
     }
 }
 
 impl Clause {
-    #[trusted] // TODO
-    #[requires(forall<i: Int> 0 <= i && i < (@self).len() ==>
-        @(@self)[i].idx < (@a).len())] // TODO: Refactor
+    // Can be made to a complete eval function if I like
+    #[requires(self.invariant((@a).len()))]
     #[requires(f.invariant())]
     #[requires(a.invariant(*f))]
-    #[ensures(result ==> self.unit(*a))] //TODO
-    //#[ensures(!result ==> !self.unit(*a))] // TODO
-    //#[ensures(result ==> !self.unsat(*a))] // TODO
-    //#[ensures(result ==> !self.sat(*a))] // TODO
+    #[ensures(result ==> self.unit(*a))] 
+    //#[ensures(!result ==> !self.unit(*a))]
+    //#[ensures(result ==> !self.unsat(*a))] 
+    //#[ensures(result ==> !self.sat(*a))] 
     pub fn check_if_unit(&self, a: &Assignments, f: &Formula) -> bool {
         let mut i: usize = 0;
         let mut unassigned: usize = 0;
         let mut k: usize = 0;
         #[invariant(loop_invariant, 0 <= @i && @i <= (@self).len())]
-        #[invariant(unass, @unassigned < 2)] // TODO: Link unassigned with Unset
-        //#[invariant(unass2, unassigned_count_clause_partial(@self, @a, @i) < 2)]
-
-        /*
-        c.vars_in_range(a.len()) &&
-            !sat_clause_inner(a, c) && 
-                exists<i: Int> 0 <= i && i < (@c).len() &&
-                    a[@(@c)[i].idx] === AssignedState::Unset && 
-                        (forall<j: Int> 0 <= j && j < (@c).len() && j != i ==>
-                            !(a[@(@c)[j].idx] === AssignedState::Unset))
-                            */
-        
-        #[invariant(is_unit, (forall<j: Int> 0 <= j && j < @i && j != @k ==>
-                !((@a)[@(@self)[j].idx] === AssignedState::Unset))
+        #[invariant(unass, @unassigned < 2)] 
+        #[invariant(k_is_unass, (@unassigned === 0 || (@a)[@(@self)[@k].idx] === AssignedState::Unset))]
+        #[invariant(kk, @unassigned > 0 ==> 
+                ((@a)[@(@self)[@k].idx] === AssignedState::Unset))]
+        #[invariant(not_sat, forall<j: Int> 0 <= j && j < @i ==>
+            match (@a)[@(@self)[j].idx] {
+                AssignedState::Positive => !(@self)[j].polarity,
+                AssignedState::Negative => (@self)[j].polarity,
+                AssignedState::Unset => @unassigned === 1,
+            }
         )]
-        #[invariant(k_is_unass, (@k === 0 || (@a)[@(@self)[@k].idx] === AssignedState::Unset))]
-        #[invariant(k2, @unassigned > 0 ==> exists<j: Int> 0 <= j && j < @i ==>
-                ((@a)[@(@self)[j].idx] === AssignedState::Unset))]
+        #[invariant(k_in_bounds, @unassigned === 0 || 0 <= @k && @k < (@self).len())]
+        #[invariant(k_only, @unassigned === 1 ==>
+                        (forall<j: Int> 0 <= j && j < @i && j != @k ==>
+                            !((@a)[@(@self)[j].idx] === AssignedState::Unset))
+        )]
+        #[invariant(k_unset, @unassigned === 0 ==> @k === 0)]
         while i < self.0.len() {
             let lit = self.0[i];
             let res = a.0[lit.idx];
             match res {
                 AssignedState::Positive => {
                     if lit.polarity {
+                        proof_assert! { sat_clause_inner(@a, *self) }
                         return false;
                     }
                 },
                 AssignedState::Negative => {
                     if !lit.polarity {
+                        proof_assert! { sat_clause_inner(@a, *self) }
                         return false;
                     }
                 },
@@ -176,23 +177,22 @@ impl Clause {
                         return false;
                     }
                     k = i;
-                    //proof_assert! { (@a)}
                     unassigned += 1;
                 },
             }
             i += 1;
         }
         if unassigned == 1 {
-            //proof_assert! { self.vars_in_range((@a).len()) }
             proof_assert! { !sat_clause_inner(@a, *self) }
+                    proof_assert! { (@a)[@(@self)[@k].idx] === AssignedState::Unset }
             proof_assert! {
-                exists<i: Int> 0 <= i && i < (@self).len() &&
-                    (@a)[@(@self)[i].idx] === AssignedState::Unset
+                 0 <= @k && @k < (@self).len() &&
+                    (@a)[@(@self)[@k].idx] === AssignedState::Unset
             }
             proof_assert! {
-                exists<i: Int> 0 <= i && i < (@self).len() &&
-                    (@a)[@(@self)[i].idx] === AssignedState::Unset && 
-                        (forall<j: Int> 0 <= j && j < (@self).len() && j != i ==>
+                 0 <= @k && @k < (@self).len() &&
+                    (@a)[@(@self)[@k].idx] === AssignedState::Unset && 
+                        (forall<j: Int> 0 <= j && j < (@self).len() && j != @k ==>
                             !((@a)[@(@self)[j].idx] === AssignedState::Unset))
             }
             true
