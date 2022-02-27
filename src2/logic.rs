3cab76f6c257b7a0cdb15ccf28d2f2b1b65128a0
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2022-02-27 01:16:19 +0100

    Cleaned up lemmas a fair amount

--- src2/logic.rs
+++ src2/logic.rs
@@ -1,7 +1,7 @@
 extern crate creusot_contracts;
-
 use creusot_contracts::*;
 use creusot_contracts::std::*;
+
 use crate::lit::*;
 use crate::clause::*;
 use crate::assignments::*;
@@ -27,6 +27,24 @@ fn flip_v(v: AssignedState) -> AssignedState {
     }
 }
 
+#[logic]
+fn pos() -> AssignedState {
+    AssignedState::Positive
+}
+
+#[logic]
+fn neg() -> AssignedState {
+    AssignedState::Negative
+}
+
+#[predicate]
+fn unset(v: AssignedState) -> bool {
+    match v {
+        AssignedState::Unset => true,
+        _ => false,
+    }
+}
+
 #[logic] 
 #[requires(f.invariant())]
 #[requires(assignments_invariant(a, f))]
@@ -44,8 +62,8 @@ pub fn lemma_not_sat_clause_implies_unsat_formula(f: Formula, c: Clause, a: Seq<
 #[logic]
 #[requires(f.invariant())]
 #[requires(@f.num_vars === a.len())]
-#[requires(0 <= ix && ix < a.len() && a[ix] === AssignedState::Unset)]
-#[requires(v === AssignedState::Positive || v === AssignedState::Negative)]
+#[requires(0 <= ix && ix < a.len() && unset(a[ix]))]
+#[requires(!unset(v))]
 #[requires(f.eventually_sat_complete_inner(a))]
 #[requires(!f.eventually_sat_complete_inner(a.set(ix, flip_v(v))))]
 #[ensures(f.eventually_sat_complete_inner(a.set(ix, v)))]
@@ -56,15 +74,17 @@ pub fn lemma_unit_forces(c: Clause, f: Formula, a: Seq<AssignedState>, ix: Int, 
 #[logic]
 #[requires(f.invariant())]
 #[requires(@f.num_vars === a.len())]
-#[requires(0 <= ix && ix < a.len() && a[ix] === AssignedState::Unset)]
-#[requires(v === AssignedState::Positive || v === AssignedState::Negative)]
+#[requires(0 <= ix && ix < a.len() && unset(a[ix]))]
+#[requires(!unset(v))]
 #[requires(c.unit_inner(a))]
 #[requires(c.in_formula(f))]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() ==> 0 <= @(@c)[j].idx && @(@c)[j].idx < a.len())]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() ==> 0 <= @(@c)[j].idx && @(@c)[j].idx < a.len())]
+#[requires(c.invariant(a.len()))]
 #[requires(exists<j: Int> 0 <= j && j < (@c).len() && @(@c)[j].idx === ix && bool_to_assignedstate(((@c)[j].polarity)) === v)]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(a[@(@c)[j].idx] === AssignedState::Unset))]
-#[requires(forall<j: Int, k: Int> 0 <= j && j < (@c).len() && k < j ==> !(@(@c)[k].idx === @(@c)[j].idx))]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(a[@(@c)[j].idx] === bool_to_assignedstate((@c)[j].polarity)))]
+//#[requires(forall<j: Int, k: Int> 0 <= j && j < (@c).len() && k < j ==> !(@(@c)[k].idx === @(@c)[j].idx))]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(@c)[j].unset_inner(a))]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(@c)[j].sat_inner(a))]
+#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> (@c)[j].unsat_inner(a))]
 #[ensures(!f.eventually_sat_complete_inner(a.set(ix, flip_v(v))))]
 #[ensures(f.unsat_inner(a.set(ix, flip_v(v))))]
 pub fn lemma_unitClauseLiteralFalse_tauNotSatisfiable(c: Clause, f: Formula, a: Seq<AssignedState>, ix: Int, v: AssignedState) {
@@ -81,33 +101,35 @@ pub fn lemma_unitClauseLiteralFalse_tauNotSatisfiable(c: Clause, f: Formula, a: 
 pub fn lemma_correctPolarityMakesClauseSat(c: Clause, a: Seq<AssignedState>, ix: Int, v: AssignedState) {}
 
 #[logic]
-#[requires(0 <= ix && ix < a.len() && a[ix] === AssignedState::Unset)]
+#[requires(!unset(v))]
+#[requires(0 <= ix && ix < a.len() && unset(a[ix]))]
 #[requires(c.unit_inner(a))]
 #[requires(!c.sat_inner(a))]
-#[requires(v === AssignedState::Positive || v === AssignedState::Negative)] 
-#[requires(exists<j: Int> 0 <= j && j < (@c).len() && @(@c)[j].idx === ix && bool_to_assignedstate((@c)[j].polarity) === v)]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() ==> 0 <= @(@c)[j].idx && @(@c)[j].idx < a.len())]
-#[requires(forall<j: Int, k: Int> 0 <= j && j < (@c).len() && k < j ==> !(@(@c)[k].idx === @(@c)[j].idx))]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(a[@(@c)[j].idx] === AssignedState::Unset))]
-#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(a[@(@c)[j].idx] === bool_to_assignedstate((@c)[j].polarity)))]
-#[ensures(forall<j: Int> 0 <= j && j < (@c).len()  ==> !((a.set(ix, v))[@(@c)[j].idx] === AssignedState::Unset))]
-#[ensures(!(a.set(ix, flip_v(v))[ix] === AssignedState::Unset))]
+#[requires(exists<j: Int> 0 <= j && j < (@c).len() && @(@c)[j].idx === ix && (@c)[j].sat_inner(a) )]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() ==> 0 <= @(@c)[j].idx && @(@c)[j].idx < a.len())]
+#[requires(c.invariant(a.len()))]
+//#[requires(forall<j: Int, k: Int> 0 <= j && j < (@c).len() && k < j ==> !(@(@c)[k].idx === @(@c)[j].idx))]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(@c)[j].unset_inner(a))]
+//#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> !(@c)[j].sat_inner(a))]
+#[requires(forall<j: Int> 0 <= j && j < (@c).len() && !(@(@c)[j].idx === ix) ==> (@c)[j].unsat_inner(a))]
+#[ensures(forall<j: Int> 0 <= j && j < (@c).len()  ==> !unset((a.set(ix, v))[@(@c)[j].idx]))]
+#[ensures(!(unset(a.set(ix, flip_v(v))[ix])))]
 #[ensures(c.unsat_inner(a.set(ix, flip_v(v))))]
 #[ensures(!c.sat_inner(a.set(ix, flip_v(v))))]
 pub fn lemma_incorrectPolarityMakesClauseUnsat(c: Clause, a: Seq<AssignedState>, ix: Int, v: AssignedState) {}
 
 #[logic]
-#[requires(0 <= ix && ix < a.len() && a[ix] === AssignedState::Unset)]
+#[requires(0 <= ix && ix < a.len() && unset(a[ix]))]
 #[requires(f.eventually_sat_complete_inner(a.set(ix, v)))]
 #[ensures(f.eventually_sat_complete_inner(a))]
 pub fn lemma_extensionSat_baseSat(f: Formula, a: Seq<AssignedState>, ix: Int, v: AssignedState) {}
 
 #[logic]
-#[requires(0 <= ix && ix < a.len() && a[ix] === AssignedState::Unset)]
-#[requires(!f.eventually_sat_complete_inner(a.set(ix, AssignedState::Positive)))]
-#[requires(!f.eventually_sat_complete_inner(a.set(ix, AssignedState::Negative)))]
+#[requires(0 <= ix && ix < a.len() && unset(a[ix]))]
+#[requires(!f.eventually_sat_complete_inner(a.set(ix, neg())))]
+#[requires(!f.eventually_sat_complete_inner(a.set(ix, pos())))]
 #[ensures(!f.eventually_sat_complete_inner(a))]
 pub fn lemma_extensionsUnsat_baseUnsat(a: Seq<AssignedState>, ix: Int, f: Formula) {
-    compatible_inner(a, a.set(ix, AssignedState::Positive));
-    compatible_inner(a, a.set(ix, AssignedState::Negative));
+    compatible_inner(a, a.set(ix, pos()));
+    compatible_inner(a, a.set(ix, neg()));
 }
