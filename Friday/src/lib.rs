ae5f49cb33b4d386362766f23b133272add4d613
Li-yao Xia <lysxia@gmail.com>
2025-10-16 20:34:25 +0200

    Update Friday

--- Friday/src/lib.rs
+++ Friday/src/lib.rs
@@ -8,22 +8,53 @@ use creusot_contracts::{std::clone::Clone, std::*, vec, *};
 // an imperative implementation of a functional prgram.
 // In other words: very naive, very slow.
 
-#[derive(Clone)]
-struct Assignments(Vec<bool>);
-struct Lit {
+pub struct Assignments(Vec<bool>);
+pub struct Lit {
     var: usize,
     value: bool,
 }
-struct Clause(Vec<Lit>);
+pub struct Clause(Vec<Lit>);
 
-#[derive(Clone)]
-struct Pasn {
+pub struct Pasn {
     assign: Assignments,
     ix: usize,
 }
 pub struct Formula {
     clauses: Vec<Clause>,
     num_vars: usize,
+}
+
+impl View for Assignments {
+    type ViewTy = Seq<bool>;
+    #[logic]
+    fn view(self) -> Self::ViewTy {
+        pearlite! { self.0@ }
+    }
+}
+
+impl Clone for Assignments {
+    #[check(terminates)]
+    #[ensures(result@ == self@)]
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl View for Clause {
+    type ViewTy = Seq<Lit>;
+
+    #[logic]
+    fn view(self) -> Self::ViewTy {
+        pearlite! { self.0@ }
+    }
+}
+
+impl Clone for Pasn {
+    #[check(terminates)]
+    #[ensures(self.eq_logic(result))]
+    fn clone(&self) -> Self {
+        Self { assign: self.assign.clone(), ix: self.ix }
+    }
 }
 
 impl Invariant for Formula {
@@ -38,7 +69,12 @@ impl Invariant for Formula {
 
 impl Formula {
     #[logic(open(self))]
-    fn sat(self, a: Assignments) -> bool {
+    pub fn num_vars(self) -> Int {
+        pearlite! { self.num_vars@ }
+    }
+
+    #[logic(open(self))]
+    pub fn sat(self, a: Assignments) -> bool {
         pearlite! {
             forall<i: Int> 0 <= i && i < self.clauses@.len() ==>
                 self.clauses@[i].sat(a)
@@ -50,8 +86,8 @@ impl Clause {
     #[logic(open(self))]
     fn vars_in_range(self, n: Int) -> bool {
         pearlite! {
-            forall<i: Int> 0 <= i && i < self.0@.len() ==>
-                self.0@[i].var_in_range(n)
+            forall<i: Int> 0 <= i && i < self@.len() ==>
+                self@[i].var_in_range(n)
         }
     }
 }
@@ -65,9 +101,9 @@ impl Lit {
     }
 
     #[logic(open(self))]
-    fn sat(self, a: Assignments) -> bool {
+    pub fn sat(self, a: Assignments) -> bool {
         pearlite! {
-            a.0@[self.var@] == self.value
+            a@[self.var@] == self.value
         }
     }
 }
@@ -78,37 +114,46 @@ impl Assignments {
         pearlite! {
             pa.assign.0@.len() == self.0@.len() &&
                 forall<i: Int> 0 <= i && i < pa.ix@ ==>
-                    pa.assign.0@[i] == self.0@[i]
+                    pa.assign@[i] == self@[i]
         }
     }
 }
 
 impl Pasn {
     #[logic(open(self))]
-    fn invariant(self, n: Int) -> bool {
+    pub fn eq_logic(self, rhs: Self) -> bool {
         pearlite! {
-            self.ix@ <= self.assign.0@.len()
-            && self.assign.0@.len() == n
+            self.assign@ == rhs.assign@ && self.ix == rhs.ix
+        }
+    }
+
+    #[logic(open(self))]
+    pub fn invariant(self, n: Int) -> bool {
+        pearlite! {
+            self.ix@ <= self.assign@.len()
+            && self.assign@.len() == n
         }
     }
 }
 
 impl Clause {
     #[logic(open(self))]
-    fn sat(self, a: Assignments) -> bool {
+    pub fn sat(self, a: Assignments) -> bool {
         pearlite! {
-            exists<i: Int> 0 <= i && i < self.0@.len() &&
-                self.0@[i].sat(a)
+            exists<i: Int> 0 <= i && i < self@.len() &&
+                self@[i].sat(a)
         }
     }
 }
 
 impl Clause {
-    #[requires(self.vars_in_range(a.0@.len()))]
+    #[check(terminates)]
+    #[requires(self.vars_in_range(a@.len()))]
     #[ensures(result == self.sat(*a))]
     fn eval(&self, a: &Assignments) -> bool {
         let mut i: usize = 0;
         let clause_len = self.0.len();
+        #[variant(clause_len@ - i@)]
         #[invariant(forall<j: Int> 0 <= j && j < i@ ==> !self.0@[j].sat(*a))]
         #[invariant(i@ <= clause_len@)]
         while i < clause_len {
@@ -122,10 +167,12 @@ impl Clause {
 }
 
 impl Formula {
-    #[requires(a.0@.len() == self.num_vars@)]
+    #[check(terminates)]
+    #[requires(a@.len() == self.num_vars@)]
     #[ensures(result == self.sat(*a))]
     fn eval(&self, a: &Assignments) -> bool {
         let mut i: usize = 0;
+        #[variant(self.clauses@.len() - i@)]
         #[invariant(forall<j: Int> 0 <= j && j < i@ ==> self.clauses@[j].sat(*a))]
         while i < self.clauses.len() {
             if !self.clauses[i].eval(a) {
@@ -137,10 +184,11 @@ impl Formula {
     }
 }
 
-#[requires(pa.ix@ < pa.assign.0@.len())]
-#[requires(pa.assign.0@.len() <= usize::MAX@)]
+#[check(terminates)]
+#[requires(pa.ix@ < pa.assign@.len())]
+#[requires(pa.assign@.len() <= usize::MAX@)]
 #[ensures(result.assign.compatible(*pa))]
-#[ensures(result.assign.0@[pa.ix@] == b)]
+#[ensures(result.assign@[pa.ix@] == b)]
 #[ensures(result.ix@ == pa.ix@ + 1)]
 fn set_next(pa: &Pasn, b: bool) -> Pasn {
     let mut new_pa = pa.clone();
@@ -149,6 +197,7 @@ fn set_next(pa: &Pasn, b: bool) -> Pasn {
     new_pa
 }
 
+#[check(terminates)]
 #[variant(f.num_vars@ - pa.ix@)]
 #[requires(pa.invariant(f.num_vars@))]
 #[requires(f.invariant())]
@@ -161,7 +210,7 @@ fn solve(f: &Formula, pa: Pasn) -> bool {
 }
 
 #[requires(f.invariant())]
-#[ensures(!result ==> forall<a: Assignments> a.0@.len() == f.num_vars@
+#[ensures(!result ==> forall<a: Assignments> a@.len() == f.num_vars()
                   ==> !f.sat(a))]
 #[ensures( result ==> exists<a: Assignments> f.sat(a))]
 pub fn solver(f: &Formula) -> bool {
